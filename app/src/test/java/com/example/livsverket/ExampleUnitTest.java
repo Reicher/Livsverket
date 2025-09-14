c562c871983920199b80c24da617035372c55080
Robin Reicher <robin.o.reicher@gmail.com>
2025-09-14 15:49:29 +0200

    Replace Kotlin test with simple Java assert test

---
+++ app/src/test/java/com/example/livsverket/ExampleUnitTest.java
@@ -0,0 +1,8 @@
+package com.example.livsverket;
+
+public class ExampleUnitTest {
+    public static void main(String[] args) {
+        assert 4 == 2 + 2 : "Math is broken";
+        System.out.println("ExampleUnitTest passed");
+    }
+}
